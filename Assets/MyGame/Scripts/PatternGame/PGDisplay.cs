f9dc97aa07587094c61afb41d83c2cc2b96c5bab
TheGreenPig <72389363+TimToller@users.noreply.github.com>
2022-05-10 19:10:46 +0200

    Minigame update

--- Assets/MyGame/Scripts/PatternGame/PGDisplay.cs
+++ Assets/MyGame/Scripts/PatternGame/PGDisplay.cs
@@ -8,22 +8,31 @@ public class PGDisplay : MonoBehaviour
     public enum DisplayStates { OFF, PENDING, ON };
     public DisplayStates state;
     public Material material;
+    public float maxEmission = 2f;
+    public float minEmission = 0f;
     public void SetOff()
     {
         state = DisplayStates.OFF;
         Color color = material.GetColor("_Color");
-        material.SetColor("_EmissionColor", color * 0);
+        material.SetColor("_EmissionColor", color * minEmission);
+        LeanTween.cancelAll();
     }
     public void SetPending()
     {
         Debug.Log("Display " + id + " is pending");
         state = DisplayStates.PENDING;
+        LeanTween.value(gameObject, minEmission, maxEmission, 0.5f).setOnUpdate((float value) =>
+        {
+            Color color = material.GetColor("_Color");
+            material.SetColor("_EmissionColor", color * value);
+        }).setLoopPingPong();
     }
     public void SetOn()
     {
         Debug.Log("Display " + id + " is on");
         state = DisplayStates.ON;
         Color color = material.GetColor("_Color");
-        material.SetColor("_EmissionColor", color * 2f);
+        material.SetColor("_EmissionColor", color * maxEmission);
+        LeanTween.cancelAll();
     }
 }
